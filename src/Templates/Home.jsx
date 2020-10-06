71ab7ef4a1f053ad0165914ba32d1c35200c145b
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-10-07 08:16:23 +0900

    push Image HomeJsx

--- src/Templates/Home.jsx
+++ src/Templates/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/styles'
+import TopImage from '../assets/images/maedagumi5.jpg'
+
+
+const useStyles = makeStyles({
+  images: {
+    maxHeight: '760px',
+  },
+  root: {
+    overflow: 'scroll'
+  }
+})
+
 
 const Home = () => {
+  const classes = useStyles()
   return (
-    <div>
-      <p>Home</p>
+    <div className={classes.root}>
+      <section >
+        <img src={TopImage} alt='' className={classes.images}/>
+      </section>
     </div>
   )
 }
