fc3f4c421a4c4577c0ab29cdfd93d79b8af025a3
”ninomiyadaisuke” <daisukeninomiya0375@gmail.com>
2020-10-06 11:08:48 +0900

    add_botton

--- src/Templates/Member1.jsx
+++ src/Templates/Member1.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 const Member1 = () => {
   return (
     <div>
       <p>大翼</p>
+      <Button variant="contained">Default</Button>
     </div>
   )
 }
