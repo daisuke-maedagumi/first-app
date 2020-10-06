0265ee5106318d2c727c978a39e9728faba003f4
”ninomiyadaisuke” <daisukeninomiya0375@gmail.com>
2020-10-06 14:40:45 +0900

    add_grid_button

--- src/Templates/Member1.jsx
+++ src/Templates/Member1.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { Button } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core';
 
 const Member1 = () => {
   return (
     <div>
       <p>大翼</p>
-      <Button variant="contained"　color="primary">Default</Button>
+      <Grid container>
+        <Grid item xs={4}>
+          <Button variant="contained"　color="primary">
+            Default
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="contained"　color="primary">
+            Default
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="contained"　color="primary">
+            Default
+          </Button>
+        </Grid>        
+      </Grid> 
     </div>
   )
 }
