b39a209e8bbe6aa883b440202a5a5fca8cdd4097
KyoheiYamamoto <arienkyohei@gmail.com>
2020-10-08 09:28:09 +0900

    photoadd

--- src/Templates/Member3.jsx
+++ src/Templates/Member3.jsx
@@ -10,6 +10,7 @@ import ListItem from '@material-ui/core/ListItem'
 // import List from '@material-ui/core/List'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import InstagramIcon from '@material-ui/icons/Instagram';
+import TopImage from "../assets/images/kyohei.jpg";
 
 import Box from '@material-ui/core/Box';
 
@@ -24,13 +25,9 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: 700,
     maxHeight: 1000,
     marginBottom: 20,
-    marginTop: 30,
+    marginTop: 100,
   },
 
-  image: {
-    width: 128,
-    height: 128,
-  },
   linkName: {
     textDecoration: "none",
     fontSize: "18px",
@@ -41,6 +38,8 @@ const useStyles = makeStyles((theme) => ({
     display: "block",
     maxWidth: "100%",
     maxHeight: "100%",
+    width: 128,
+    height: 128,
   },
   hugehoge: {
     alignItems: "stretch",
@@ -49,6 +48,9 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "stretch",
   },
+  linkurl: {
+    color: 'blue',
+  }
 }));
 
 export default function Member3()
@@ -62,75 +64,86 @@ export default function Member3()
         <Grid container spacing={2}>
           <Grid item>
             <ButtonBase className={classes.image}>
-              <img
+              {/* <img
                 className={classes.img}
                 alt="complex"
-                src="/static/images/grid/complex.jpg"
-              />
+                src="../assets/images/maedagumi5.jpg"
+              /> */}
+              <img src={TopImage} alt="complex" className={classes.img} />
             </ButtonBase>
           </Grid>
           <Grid item xs={12} sm container>
             <Grid item xs container direction="column" spacing={2}>
               <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
+                <Typography gutterBottom variant="subtitle1" marginTop="100">
                   <Grid container className="hugehoge">
-                    <h1>自己紹介</h1>
+                    <h2>自己紹介</h2>
                   </Grid>
                   <Grid container className="hugehoge">
-                    <p>名前：山本恭平（やまもときょうへい）</p>
+                    <li>名前：山本恭平（やまもときょうへい）</li>
                   </Grid>
                   <Grid container className="hugehoge">
-                    <p>出身地：福岡県北九州市</p>
+                    <li>出身地：福岡県北九州市</li>
                   </Grid>
                   <Grid container className="hugehoge">
-                    <p>年齢：24</p>
+                    <li>年齢：24</li>
                   </Grid>
                   <Grid container className="hugehoge">
-                    <p>学歴：福岡工業大学卒業</p>
+                    <li>趣味：スポーツ観戦(野球・サッカー)など</li>
+                  </Grid>
+                  <Grid container className="hugehoge">
+                    <h2>持っている資格</h2>
+                  </Grid>
+                  <Grid container className="hugehoge">
+                    <li>MOSWord2013</li>
+                  </Grid>
+                  <Grid container className="hugehoge">
+                    <li>MOSExcel2013</li>
                   </Grid>
                 </Typography>
               </Grid>
               <Grid item>
                 <Grid container className="hugehoge">
-                  <h2>資格</h2>
-                </Grid>
-              </Grid>
-              <Grid container className="hugehoge">
-                <li>MOSWord2013</li>
-              </Grid>
-              <br/>
-              <Grid container className="hugehoge">
-                <li>MOSExcel2013</li>
-              </Grid>
-              <Grid item>
-                <Grid container className="hugehoge">
                   <h2>紹介文</h2>
                 </Grid>
-                <Grid container className="hugehoge">
-                  長い文章を書く予定
+                {/* <Box display="flex" flexDirection="row" p={1} m={1} bgcolor="background.paper">
+                </Box> */}
+
+                <Grid container className="hugehoge" xs="true" direction="row">
+                  <p>
+                    2019年3月大学を卒業（情報システム工学を専攻していました）
+                    学外ではTABIPPOという団体で旅を広める活動や教育支援団体で教育支援などを行ってきました
+                    現在はプログラミングが好きです（オンラインサロンでも勉強したり教えあったりしています・アウトプットの重要性を実感してます）
+                  </p>
                 </Grid>
               </Grid>
-              <br />
               <Grid container className="hugehoge">
-                <h2>リンク先</h2>
+                <h3>SNS</h3>
               </Grid>
               <Grid container className="hugehoge">
-                <Box display="flex" justifyContent="stretch" className="hugehoge" >
+                <Box
+                  display="flex"
+                  justifyContent="stretch"
+                  className="hugehoge"
+                >
                   <ListItem
                     component="a"
-                    href="https://github.com/KyoheiYamamoto">
+                    href="https://github.com/KyoheiYamamoto"
+                  >
                     <GitHubIcon />
                     <ListItemText primary="Github" />
                   </ListItem>
                   <ListItem
                     component="a"
-                    href="https://twitter.com/davidkyohei" >
+                    href="https://twitter.com/davidkyohei"
+                  >
                     <TwitterIcon />
                     <ListItemText primary="Twitter" />
                   </ListItem>
                   <ListItem
                     component="a"
-                    href="https://www.instagram.com/kyohei_1642" >
+                    href="https://www.instagram.com/kyohei_1642"
+                  >
                     <InstagramIcon />
                     <ListItemText primary="Instagram" />
                   </ListItem>
