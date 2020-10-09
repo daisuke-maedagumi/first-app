01a3bf21c8e6b18717657838c5d9e0a09de485da
yamaguchiosamu <66462182+yamaguchiosamu@users.noreply.github.com>
2020-10-09 10:50:33 +0900

    chengemember5

--- src/Templates/Member5.jsx
+++ src/Templates/Member5.jsx
@@ -1,11 +1,136 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItem from '@material-ui/core/ListItem'
+// import List from '@material-ui/core/List'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TopImage from "../assets/images/ys.jpg";
 
-const Member5 = () => {
+import Box from '@material-ui/core/Box';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 700,
+    maxHeight: 1000,
+    marginBottom: 20,
+    marginTop: 100,
+  },
+
+  linkName: {
+    textDecoration: "none",
+    fontSize: "18px",
+    color: "orange",
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    width: 128,
+    height: 128,
+  },
+  hugehoge: {
+    alignItems: "stretch",
+    justify: "space-between",
+    direction: "row",
+    display: "flex",
+    justifyContent: "stretch",
+  },
+  linkurl: {
+    color: 'blue',
+  }
+}));
+
+export default function Member5()
+{
+  const classes = useStyles();
+
+
   return (
-    <div>
-      <h1>Member5</h1>
-    </div>
-  )
-}
+    <div className={classes.hugehoge}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              {/* <img
+                className={classes.img}
+                alt="complex"
+                src="../assets/images/maedagumi5.jpg"
+              /> */}
+              <img src={TopImage} alt="complex" className={classes.img} />
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" >
+                  <Grid container className="hugehoge">
+                    <h2>自己紹介</h2>
+                  </Grid>
+                  <Grid container className="hugehoge">
+                    <li>名前：山本晋太朗（やまもとしんたろう）</li>
+                  </Grid>
+                  <Grid container className="hugehoge">
+                    <li>出身地：福岡県北九州市</li>
+                  </Grid>
+                  <Grid container className="hugehoge">
+                    <li>年齢：26</li>
+                  </Grid>
+                  <Grid container className="hugehoge">
+                    <li>趣味：銭湯、スケートボード</li>
+                  </Grid>
 
-export default Member5
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Grid container className="hugehoge">
+                  <h2>紹介文</h2>
+                </Grid>
+                {/* <Box display="flex" flexDirection="row" p={1} m={1} bgcolor="background.paper">
+                </Box> */}
+
+                <Grid container className="hugehoge"  direction="row">
+                  <p>
+                  大学大学院では主に物理系の研究をしていました。全く楽しくなかったので中退してアフリカに行く予定でしたが、コロナの影響で行けなくなりました。
+                  銭湯が好きです。
+                  </p>
+                </Grid>
+              </Grid>
+              <Grid container className="hugehoge">
+                <h3>SNS</h3>
+              </Grid>
+              <Grid container className="hugehoge">
+                <Box
+                  display="flex"
+                  justifyContent="stretch"
+                  className="hugehoge"
+                >
+
+                  <ListItem
+                    component="a"
+                    href="@monkeytatata"
+                  >
+                    <InstagramIcon />
+                    <ListItemText primary="Instagram" />
+                  </ListItem>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
