1b95d7114188e2c3a95607c15fca2539afa60e83
”ninomiyadaisuke” <daisukeninomiya0375@gmail.com>
2020-10-14 09:09:28 +0900

    トラハック実践６add_profile＿photo

--- src/components/Chatbot/Chat.jsx
+++ src/components/Chatbot/Chat.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
+import NoProfile from '../../assets/images/no-profile.png';
+import Kyohei from '../../assets/images/kyohei.jpg';
 
 
 const Chat = (props) => {
     const isQuestion = (props.type === 'question')
-    const classes = isQuestion ? 'p-chat＿row' : 'p-chat＿reverse'
+    const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
     return (
-        <ListItem>
+        <ListItem className={classes}>
             <ListItemAvatar>
-            <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+                {isQuestion ? (
+                    <Avatar alt="icon" src={Kyohei} />
+                ) : (
+                    <Avatar alt="icon" src={NoProfile} />
+                ) }
+            
+            
             </ListItemAvatar>
     <div className="p-chat__bubble">{props.text}</div>
             
